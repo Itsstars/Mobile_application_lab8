c86d38d683036a599735f39f616aab5d4a4c8db2
najma <najmahassan1212@hotmail.com>
2020-12-29 22:58:40 +0800

    First commit

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:0e3fdff9-4088-4753-bf99-fa91bf0ba157",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_JKyPXpeTm",
+                        "AppClientId": "3t4638du6nligg25mg8354b8a5",
+                        "AppClientSecret": "ha2gnelcrja6gbh22tpn8o5cnq0u182dsektaba8etka067a7ie",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
